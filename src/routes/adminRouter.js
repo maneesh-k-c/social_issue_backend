6581969893776bee6f5ab75def099f3a55415e63
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-03-22 17:18:06 +0530

    department

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,6 +3,7 @@ const AdminRouter = express.Router()
 const register = require('../models/userData')
 const login = require('../models/loginData')
 const company = require('../models/companyData')
+const department = require('../models/departmentData')
 var objectId = require('mongodb').ObjectID;
 
 AdminRouter.use(express.static('./public'))
@@ -123,6 +124,62 @@ AdminRouter.get("/delete_company/:id", async (req, res) => {
 
 });
 
+AdminRouter.get("/view-department", async (req, res) => {
+    try {
+        login.aggregate([
+            {
+                '$lookup': {
+                    'from': 'department_tbs',
+                    'localField': '_id',
+                    'foreignField': 'login_id',
+                    'as': 'data'
+                }
+            },
+            {
+                "$unwind": "$data"
+            },
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "department_name": { "$first": "$data.department_name" },
+                    "address": { "$first": "$data.address" },
+                    "phone": { "$first": "$data.phone" },
+                    "description": { "$first": "$data.description" },
+                    "status": { "$first": "$status" }
+                }
+            }
+
+        ]).then((department) => {
+            res.render('all-department', { department })
+        })
+    } catch (err) {
+
+    }
+
+});
+
+AdminRouter.get("/approve_department/:id", async (req, res) => {
+    const id = req.params.id
+    console.log("id==>",id);
+    login.findByIdAndUpdate({ _id: id }, { $set: { status: 1 } }).then((details) => {
+        console.log("details==>", details);
+        res.redirect('/admin/view-department')
+    })
+
+});
+
+AdminRouter.get("/delete_department/:id", async (req, res) => {
+    const id = req.params.id
+    login.deleteOne({ _id: id }).then((details) => {
+        console.log("details==>",details.deletedCount);
+       if(details.deletedCount===1){
+        department.deleteOne({ login_id: id }).then((details) => {
+            res.redirect('/admin/view-department')
+        })
+       }
+    })
+
+});
 
 
 
